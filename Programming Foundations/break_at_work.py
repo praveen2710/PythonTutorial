85938460840c5e33b73cb9f69bf12e293103fd81
Praveen Banthia <pbanthia@masonlive.gmu.edu>
2016-12-11 21:48:38 -0600

    added while loop example

---
+++ Programming Foundations/break_at_work.py
@@ -0,0 +1,13 @@
+import webbrowser
+import time
+
+total_breaks = 3
+break_count = 0
+
+print("this program started on "+time.ctime())
+
+while(break_count < total_breaks):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=8Vrd5D8iQYc")
+    break_count = break_count + 1
+    
