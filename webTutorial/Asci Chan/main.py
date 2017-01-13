2ef4fd846b580b9d518d77b6e3ba5a743d61e8ee
Praveen Banthia <pbanthia@masonlive.gmu.edu>
2017-01-13 02:44:56 -0600

    Aschi Chan,Vagrant

---
+++ webTutorial/Asci Chan/main.py
@@ -0,0 +1,67 @@
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import webapp2
+import os
+import jinja2
+
+import db
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+#autoescape escapes all input coming from broser and you can do {{items | safe}} to disable that
+jinja_env = jinja2.Environment(loader= jinja2.FileSystemLoader(template_dir),autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Art(db.Model):
+    title = db.StringProperty(required=True)
+    # this is done due to StringProperty size limitation
+    art = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+class MainPage(Handler):
+
+    def render_front(self,title="",art="",error=""):
+        arts = db.GqlQuery("select * from Art"
+                          "ORDER BY created DESC")
+        self.render('front.html',title=title,art=art,error=error,arts=arts)
+
+    def get(self):
+        self.render('front.html')
+
+    def post(self):
+        title = self.request.get('title')
+        art = self.request.get('art')
+
+        if(title and art):
+            a = Art(title = title , art = art)
+            a.put()
+
+            self.redirect("/")
+        else:
+            error = "We need both title and some artwork"
+            self.render_front(title=title,art=art,error=error)
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
